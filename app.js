de7c94369aeaed513e728a784bcdab23c82806f4
Dev17-ultroNeous <dev17.ultroneous@gmail.com>
2022-05-05 20:13:05 +0530

    final-change

--- app.js
+++ app.js
@@ -6,6 +6,18 @@ const app = express();
 const mainRoute = require("./routes/mainRoute")
 const viewRoute = require("./routes/viewRoute")
 const ErrorController = require("./controllers/errorController");
+// const https = require("https")
+// const http = require("http")
+// const fs = require("fs")
+// const privateKey = fs.readFileSync('/etc/letsencrypt/live/ultroneous.com/privkey.pem', 'utf8');
+// const certificate = fs.readFileSync('/etc/letsencrypt/live/ultroneous.com/cert.pem', 'utf8');
+// const ca = fs.readFileSync('/etc/letsencrypt/live/ultroneous.com/chain.pem', 'utf8');
+
+// const credentials = {
+//     key: privateKey,
+//     cert: certificate,
+//     ca: ca
+// };
 app.use(cors());
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 
@@ -33,6 +45,15 @@ app.set('view engine', 'ejs');
 
 app.use('/admin', viewRoute)
 app.use(ErrorController);
-const port = process.env.PORT || 8000;
-app.listen(port)
-app.listen(console.log('server listening at port ' + port));
+
+
+// const port = process.env.PORT || 8000;
+// const httpServer = http.createServer(app);
+// const httpsServer = https.createServer(credentials, app);
+
+// httpsServer.listen(8000, () => {
+//     console.log('HTTPS Server running on port 8000');
+// });
+
+// app.listen(port)
+// app.listen(console.log('server listening at port ' + port));
